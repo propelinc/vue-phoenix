27a72e5fd377557cd396d1c52fda611edb21725f
Ram Mehta <ram.mehta@gmail.com>
2020-05-27 18:32:49 -0400

    Export plugin type.

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
-import CmsPlugin from './src/plugins/cms';
-import PlanoutPlugin, { PlanoutOverrides as _PlanoutOverrides } from './src/plugins/planout';
-import { PlanoutConfig as _PlanoutConfig } from 'planout';
+import VueCmsPlugin from './src/plugins/cms';
+import VuePlanoutPlugin, {
+  PlanoutPlugin as PlanoutPluginType,
+  PlanoutOverrides as PlanoutOverridesType
+} from './src/plugins/planout';
+import { PlanoutConfig as PlanoutConfigType } from 'planout';
 import { Content, HTMLContent } from './src/api';
 
 export { getClosest } from './src/utils';
@@ -8,10 +11,11 @@ export { planout } from './src/plugins/planout';
 
 export type CmsContent = Content;
 export type CmsHtmlContent = HTMLContent;
-export type PlanoutOverrides = _PlanoutOverrides;
-export type PlanoutConfig = _PlanoutConfig;
+export type PlanoutOverrides = PlanoutOverridesType;
+export type PlanoutPlugin = PlanoutPluginType;
+export type PlanoutConfig = PlanoutConfigType;
 
 export default {
-  cms: CmsPlugin,
-  planout: PlanoutPlugin,
+  cms: VueCmsPlugin,
+  planout: VuePlanoutPlugin,
 };
