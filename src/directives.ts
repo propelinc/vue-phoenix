3446d2d544431113a7abea2cf2e4ae5968da2bd6
Ram Mehta <ram.mehta@gmail.com>
2020-11-17 15:38:39 -0500

    Another whack at inf scroll zone/

--- src/directives.ts
+++ src/directives.ts
@@ -25,15 +25,19 @@ const infiniteScroll: DirectiveOptions = {
       : { action: binding.value, enabled: true };
 
     if (!params.enabled) {
+      console.info('DISABLED inf scrolling');
       return;
     }
 
     const tolerance = parseInt(binding.arg || '100', 10);
     const action = params.action;
 
+    console.info('SETUP inf scrolling');
     el.addEventListener('scroll', (): void => {
+      console.info('SCROLLEVENT inf scrolling');
       const height = (el.firstChild as DestroyHTMLElement).clientHeight;
       if (el.scrollTop >= height - el.clientHeight - tolerance) {
+        console.info('CALC SUCCESS inf scrolling');
         action();
       }
     });
