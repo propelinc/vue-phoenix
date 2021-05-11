7ddc69eb1aa5e8ba581ada28dbe5fd9e7906317d
Jonathan Williams <jonathan@joinpropel.com>
2021-05-10 22:59:16 -0700

    removing some package dependencies and config to handle those dependencies

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.tsx?$': 'ts-jest',
   },
-  transformIgnorePatterns: ['node_modules/(?!vue-material-design-icons)'],
+  transformIgnorePatterns: ['/node_modules/'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
